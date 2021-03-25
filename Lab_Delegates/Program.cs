cf5c8ca985a33a2d4926ca4b91f090438a487c1e
Mikhail200119 <kovalevich.misha@inbox.ru>
2021-03-25 20:10:49 +0300

    Delegates Lab

---
+++ Lab_Delegates/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+
+namespace Lab_Delegates
+{
+    public delegate void Delegate();
+
+    class Ping
+    {
+        private static int Counter = 1;
+
+        public static void PrintPing()
+        {
+            Console.Write($"{Counter}. Pong received Ping\n");
+            Counter += 2;
+            Thread.Sleep(360);
+        }
+    }
+
+    class Pong
+    {
+        private static int Counter = 2;
+
+        public static void PrintPong()
+        {
+            Console.Write($"{Counter}. Ping received Pong\n");
+            Counter += 2;
+            Thread.Sleep(360);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Delegate del;
+            Random random = new Random();
+            del = new Delegate(Ping.PrintPing);
+            del += Pong.PrintPong;
+            int i = 0;
+            while (i < 3)
+            {
+                del();
+                i++;
+            }
+        }
+    }
+}
