e2637eb1b12d8af59c2c9af2b44e856eb54a0fc0
Mikhail200119 <kovalevich.misha@inbox.ru>
2021-03-27 20:50:22 +0300

    Delegates

--- Lab_Delegates/Program.cs
+++ Lab_Delegates/Program.cs
@@ -2,8 +2,20 @@
 using System.Threading;
 using PingPong;
 
+/*Напишите "пинг-понг":
+•	2 класса Ping и Pong
+•	один уведомляет другого, о том, что "произошёл пинг", другой - о том, что "произошёл понг",
+•	одна пара объектов "играют" между собой, другая пара - между собой и т.д.
+и выводить на консоль соответсвующие сообщения, что-то вроди такого:
+1.	Ping received Pong.
+2.	Pong received Ping.
+3.	Ping received Pong.
+4.	Pong received Ping.
+5.	Ping received Pong.
+*/
+
 namespace Lab_Delegates
-{
+{ 
     class Program
     {
         static void StartGame(Ping A, Pong B)
